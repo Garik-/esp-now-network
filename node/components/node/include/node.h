4c1fff28d197182449f8a206cfbb25bb7f615c83
Gar|k <garik.djan@gmail.com>
2026-02-03 22:49:06 +0300

    feat: some optimization

--- node/components/node/include/node.h
+++ node/components/node/include/node.h
@@ -1,4 +1,9 @@
-#pragma once
+#ifndef __NODE_H__
+#define __NODE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "esp_err.h"
 #include "esp_now.h"
@@ -6,5 +11,26 @@
 
 typedef esp_now_send_status_t node_send_status_t;
 
+/**
+ * @brief Initialize ESP-NOW node
+ * @param channel WiFi channel (1-13)
+ * @param mac Optional MAC address, NULL to use default
+ * @return ESP_OK on success
+ */
 esp_err_t node_init(uint8_t channel, const uint8_t *mac);
+
+/**
+ * @brief Send broadcast message
+ * @param data Payload data
+ * @param len Payload length
+ * @param out_status Send status (ESP_NOW_SEND_SUCCESS or ESP_NOW_SEND_FAIL)
+ * @param xTicksToWait Timeout in FreeRTOS ticks
+ * @return ESP_OK on success, ESP_ERR_TIMEOUT on timeout
+ */
 esp_err_t node_broadcast(const uint8_t *data, size_t len, node_send_status_t *out_status, TickType_t xTicksToWait);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NODE_H__ */
--- node/components/node/node.c
+++ node/components/node/node.c
@@ -11,11 +11,12 @@
 #include "include/node.h"
 
 static const char *TAG = "NODE";
+static const uint8_t broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 TaskHandle_t xTaskToNotify = NULL;
 
 #define TRY(expr) ESP_RETURN_ON_ERROR((expr), TAG, "%s:%d", __func__, __LINE__)
 
-static esp_err_t wifi_init(uint8_t channel, const uint8_t *mac) {
+__attribute__((cold)) static esp_err_t wifi_init(uint8_t channel, const uint8_t *mac) {
     TRY(esp_netif_init());
     TRY(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -32,7 +33,7 @@ static esp_err_t wifi_init(uint8_t channel, const uint8_t *mac) {
     return ESP_OK;
 }
 
-static esp_err_t nvs_init() {
+__attribute__((cold)) static esp_err_t nvs_init() {
     esp_err_t ret = nvs_flash_init();
     if (unlikely(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)) {
         TRY(nvs_flash_erase());
@@ -60,42 +61,37 @@ static void send_cb(const esp_now_send_info_t *tx_info, esp_now_send_status_t st
 }
 
 esp_err_t node_broadcast(const uint8_t *data, size_t len, esp_now_send_status_t *out_status, TickType_t xTicksToWait) {
-    static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-    __atomic_store_n(&xTaskToNotify, xTaskGetCurrentTaskHandle(),
-                     __ATOMIC_SEQ_CST); // TODO: фигово наверное если часто вызывается или вызывается из разных потоков
+    __atomic_store_n(&xTaskToNotify, xTaskGetCurrentTaskHandle(), __ATOMIC_SEQ_CST);
 
     esp_err_t err = esp_now_send(broadcast_mac, data, len);
-    if (err != ESP_OK) {
+    if (unlikely(err != ESP_OK)) {
         return err;
     }
 
     BaseType_t xResult = xTaskNotifyWait(pdFALSE, ULONG_MAX, (uint32_t *)out_status, xTicksToWait);
 
-    if (xResult != pdPASS) {
+    if (unlikely(xResult != pdPASS)) {
         return ESP_ERR_TIMEOUT;
     }
 
     return ESP_OK;
 }
 
-static esp_err_t espnow_init(uint8_t channel) {
+__attribute__((cold)) static esp_err_t espnow_init(uint8_t channel) {
     TRY(esp_now_init());
     TRY(esp_now_register_send_cb(send_cb));
 
-    esp_now_peer_info_t peer = {
-        .channel = channel,
-        .ifidx = ESP_IF_WIFI_STA,
-        .encrypt = false,
-        .peer_addr = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-    };
+    const esp_now_peer_info_t peer = {.channel = channel,
+                                      .ifidx = ESP_IF_WIFI_STA,
+                                      .encrypt = false,
+                                      .peer_addr = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}};
 
     TRY(esp_now_add_peer(&peer));
 
     return ESP_OK;
 }
 
-esp_err_t node_init(uint8_t channel, const uint8_t *mac) {
+__attribute__((cold)) esp_err_t node_init(uint8_t channel, const uint8_t *mac) {
     TRY(nvs_init());
     TRY(wifi_init(channel, mac));
     TRY(espnow_init(channel));
