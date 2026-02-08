18f2a74060c0ed5208265535a1145a52a033407b
Gar|k <garik.djan@gmail.com>
2026-02-08 18:06:50 +0300

    feat: move to config

--- gateway/main/main.c
+++ gateway/main/main.c
@@ -16,6 +16,8 @@ static const char *TAG = "main_gateway";
 
 static esp_mqtt_client_handle_t s_client = NULL;
 
+#define TRY(expr) ESP_RETURN_ON_ERROR((expr), TAG, "%s:%d", __func__, __LINE__)
+
 __attribute__((cold)) static esp_err_t nvs_init() {
     esp_err_t ret = nvs_flash_init();
     if (unlikely(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)) {
@@ -51,18 +53,18 @@ espnow_rx_handler_t handle(const espnow_rx_t *rx) {
     char topic[27];
     snprintf(topic, sizeof(topic), "/device/" MACSTR "", MAC2STR(rx->mac_addr));
 
-    int msg_id = esp_mqtt_client_publish(s_client, topic, (const char *)rx->data, rx->len, 0, 1);
+    int msg_id = esp_mqtt_client_publish(s_client, topic, (const char *)rx->data, rx->len, GATEWAY_BROKER_QOS,
+                                         GATEWAY_BROKER_RETAIN);
     ESP_LOGI(TAG, "publish message, topic=%s len=%d msg_id=%d", topic, rx->len, msg_id);
 
     return ESP_OK;
 }
 
 esp_err_t app_run() {
-    ESP_RETURN_ON_ERROR(nvs_init(), TAG, "nvs_init");
-    ESP_RETURN_ON_ERROR(with_closer(wifi_start, NULL), TAG, "wifi_start");
-    ESP_RETURN_ON_ERROR(with_closer(espnow_start, &handle), TAG, "espnow_start");
-
-    ESP_RETURN_ON_ERROR(mqtt_app_start(), TAG, "mqtt_app_start");
+    TRY(nvs_init());
+    TRY(with_closer(wifi_start, NULL));
+    TRY(with_closer(espnow_start, &handle));
+    TRY(mqtt_app_start());
 
     return ESP_OK;
 }
